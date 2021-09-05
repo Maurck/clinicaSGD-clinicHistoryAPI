d2c548f4aedee98f252d77c12aed8db283feeabf
Maurk <mauricio.lp@astrumla.com>
2021-09-05 18:55:30 -0500

    feat: create_clinic_history API body fields required

--- apis/history/create_history/create_history_validator.py
+++ apis/history/create_history/create_history_validator.py
@@ -1,6 +1,43 @@
 from utils.utils import validate_parameters
 
-create_history_body_schema = {}
+create_history_body_schema = {
+    "patient_number":{
+        "required": True,
+        "nullable": False
+    },
+    "patient_name": {
+        "required": True,
+        "nullable": False
+    },
+    "patient_dni": {
+        "required": True,
+        "nullable": False  
+    },
+    "patient_birth_date": {
+        "required": True,
+        "nullable": False  
+    },
+    "patient_age": {
+        "required": True,
+        "nullable": False  
+    },
+    "patient_sex": {
+        "required": True,
+        "nullable": False  
+    },
+    "medic_name": {
+        "required": True,
+        "nullable": False  
+    },
+    "diagnostic": {
+        "required": True,
+        "nullable": False  
+    },
+    "admision_date": {
+        "required": True,
+        "nullable": False  
+    }
+}
 
 class CreateHistoryValidator:
 
